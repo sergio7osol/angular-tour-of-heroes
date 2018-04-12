4439e7a35ac26d852bc8e00e6dcb9f7ace9d51bd
Sergey V Osokin SVO <sergey8osokin@gmail.com>
2018-04-13 00:55:14 +0300

    Displaying a list

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../Hero';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heros',
@@ -8,12 +9,12 @@ import { Hero } from '../Hero';
 })
 
 export class HerosComponent implements OnInit {
-  
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-    birthday: new Date()
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
